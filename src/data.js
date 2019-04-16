c6ccefa99f8dd92b61c004c184d9283b81aa5135
LeslieZarate <fiorelly30zarate@gmail.com>
2019-04-16 15:13:43 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,29 @@
 const getSearchData = (data) => {
-  const dataMovie = data.map(data => {
+	const dataMovie = data.map(data => {
     return { Title: data.Title, Poster: data.Poster, Year: data.Year };
-  });
-  return dataMovie;
+	});
+	return dataMovie;
 };
+
 const getId = (data) => {
-  const dataMovie = data.map(data => data.imdbID);
-  return dataMovie;
+ const dataMovie = data.map(data => data.imdbID);
+ return dataMovie;
 };
+
+const getItemDetails = (data) => {
+	const details = {Title: data.Title, Year: data.Year, Runtime: data.Runtime, Genre: data.Genre, Actors: data.Actors, Plot: data.Plot, Poster: data.Poster, imdbRating: data.imdbRating};
+	return details;
+}
+
 const sortData = (data) => {
-  const dataOrder = [...data];
-  let orderedMovie = dataOrder.sort((eleA, eleB) => eleB.imdbRating - eleA.imdbRating);    
-  return orderedMovie;
+ const dataOrder = [...data];
+ let orderedMovie = dataOrder.sort((eleA, eleB) => eleB.imdbRating - eleA.imdbRating);
+ return orderedMovie;
 };
 
 window.movie = {
   getSearchData,
   getId,
+  getItemDetails,
   sortData
 };
