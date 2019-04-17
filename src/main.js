7a0f33b78dbf9dfbb39484dbf04be6c2a50278dc
lorenalugo <lugotlorenay@gmail.com>
2019-04-16 20:27:56 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ btnSearch.addEventListener('click', () => {
 });
 
  result.addEventListener('click', (event) => {
-   const url = `https://www.omdbapi.com/?&${apiKey}&t=${event.target.alt}`;
+   const url = `//www.omdbapi.com/?&${apiKey}&t=${event.target.alt}`;
    console.log(fetchDataSearch(url));
  });
 
@@ -33,7 +33,7 @@ const fetchDataSearchTotal = (url) => {
    .then(dataIds =>{
      let newData = [];
      for (let i = 0; i < dataIds.length;i++) {
-       newData.push(fetch(`http://www.omdbapi.com/?&${apiKey}&i=${dataIds[i]}`).then(response=>response.json()));
+       newData.push(fetch(`//www.omdbapi.com/?&${apiKey}&i=${dataIds[i]}`).then(response=>response.json()));
      }
      Promise.all(newData)
        .then(responses =>{
